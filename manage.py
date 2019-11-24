0626973578652f63e9d88e6b9ac61c689f7ca903
Álvaro Camacho <mauricio-xxi@hotmail.com>
2019-11-24 18:16:43 +0100

    add user model to manage file

--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
 from app.main import create_app, db
+from app.main.model import user
 
 app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
 
@@ -15,6 +16,7 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 
 manager.add_command('db', MigrateCommand)
+
 
 @manager.command
 def run():
